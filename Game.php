63eabbb819789c1b0c5ee2c1fc0d40e1279eb0da
Aurélien Fontaine <afontaine@efidev.com>
2013-12-17 13:52:23 +0100

    Init

---
+++ Game.php
@@ -0,0 +1,7 @@
+<?php
+namespace Conway\GameOfLife;
+
+
+class Game {
+
+} 
---
+++ Tests/GameTest.php
@@ -0,0 +1,18 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ *
+ * @category    PhpStorm
+ * @author     aurelien
+ * @copyright  2013 Efidev
+ * @version    CVS: Id:$
+ */
+namespace Conway\GameOfLife\Tests;
+
+
+class GameTest extends \PHPUnit_Framework_TestCase
+{
+
+}
+ 
